6a4b61ac7812d5045e9cdea7a4a242c4af04debd
MiguelCabero <Miguel.Cabero@solera.com>
2022-11-02 16:57:58 +0100

    First call to the CITIES API

--- fligths/src/Components/CountryListComponent/CountryList.js
+++ fligths/src/Components/CountryListComponent/CountryList.js
@@ -1,10 +1,35 @@
-import Country from './Country';
-import './CountryList.component.css';
+import axios from "axios";
+import { React, useState, useEffect } from "react";
+import Country from "./Country";
+import "./CountryList.component.css";
+
 function CountryList(props) {
-	return (
-		<div className='countryList'>
-			<h2>Select your {props.action}</h2>
-			<Country
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    callCities();
+  });
+
+  function callCities() {
+    axios
+      .get("http://localhost:8084/api/cities", {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      .then((response) => {
+        setCities(response.data, []);
+      });
+  }
+
+  return (
+    <div className="countryList">
+      <h2>Select your {props.action}</h2>
+
+      {cities.map((city) => (
+        <Country name={city.name} countryCode={city.country} />
+      ))}
+      {/* <Country
 				name='Madrid'
 				countryCode='ES'
 			/>
@@ -19,9 +44,9 @@ function CountryList(props) {
 			<Country
 				name='London'
 				countryCode='GB'
-			/>
-		</div>
-	);
+			/> */}
+    </div>
+  );
 }
 
 export default CountryList;
