f73db16c166040babc3c86b886a61f7a1f5cf801
MiguelCabero <Miguel.Cabero@solera.com>
2022-11-03 12:08:29 +0100

    Filter by airline

--- fligths/src/Components/ResultsFormComponent/Filters.js
+++ fligths/src/Components/ResultsFormComponent/Filters.js
@@ -1,48 +1,36 @@
-import ToggleCheck from '../ToggleCheck';
-import './Filters.component.css';
+import ToggleCheck from "../ToggleCheck";
+import "./Filters.component.css";
 
 function Filters(props) {
-	return (
-		<div className='results-filters'>
-			<form className='filters-form'>
-				<h3>Filters:</h3>
-				<label htmlFor='Company-filter'>Airline: </label>
-				<select
-					name='Company-filter'
-					id='company-filter'>
-					{props.airlines &&
-						props.airlines.map((airline, index) => (
-							<option
-								value={airline.id}
-								key={index}>
-								{airline.airlineName}
-							</option>
-						))}
-				</select>
-				<label htmlFor='Date-filter'>Date: </label>
-				<select
-					name='Date-filter'
-					id='date-filter'>
-					{props.dates &&
-						props.dates.map((date, index) => (
-							<option
-								value={date.date_selected}
-								key={index}>
-								{date.date_selected}
-							</option>
-						))}
-				</select>
-				<ToggleCheck
-					label='Layover'
-					action='Layover'
-				/>
-				<ToggleCheck
-					label='Luggage'
-					action='Luggage'
-				/>
-			</form>
-		</div>
-	);
+  let set = new Set(props.airlines.map((obj) => obj.airlineName));
+  set = Array.from(set);
+  return (
+    <div className="results-filters">
+      <form className="filters-form">
+        <h3>Filters:</h3>
+        <label htmlFor="Company-filter">Airline: </label>
+        <select name="Company-filter" id="company-filter">
+          {props.airlines &&
+            set.map((airline, index) => (
+              <option value={airline} key={index}>
+                {airline}
+              </option>
+            ))}
+        </select>
+        <label htmlFor="Date-filter">Date: </label>
+        <select name="Date-filter" id="date-filter">
+          {props.dates &&
+            props.dates.map((date, index) => (
+              <option value={date.date_selected} key={index}>
+                {date.date_selected}
+              </option>
+            ))}
+        </select>
+        <ToggleCheck label="Layover" action="Layover" />
+        <ToggleCheck label="Luggage" action="Luggage" />
+      </form>
+    </div>
+  );
 }
 
 export default Filters;
