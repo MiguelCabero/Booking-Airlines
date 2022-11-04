fb2b4dd38be5a99149e226a601e2615f2e1288c7
LeonAngelit <81989897+LeonAngelit@users.noreply.github.com>
2022-11-04 12:06:31 +0100

    first selenium test

--- citiesapi/src/main/resources/data.sql
+++ citiesapi/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into `cities` (ID,name,country,airline) values (1,'Madrid','ES',1),(2,'Barcelona','ES',1),(3,'Sevilla','ES',1),(4,'Paris','FR',2),(5,'London','GB',3),(6,'Berlin','DE',4),(7,'Rome','IT',5),(8,'New York','US',6);
+insert into `cities` (ID,name,country,airline) values (1,'Madrid','ES',1),(2,'Barcelona','ES',1),(3,'Seville','ES',1),(4,'Paris','FR',2),(5,'London','GB',3),(6,'Berlin','DE',4),(7,'Rome','IT',5),(8,'New York','US',6);
 
 
--- selenium/src/test/java/selenium/TestSelenium.java
+++ selenium/src/test/java/selenium/TestSelenium.java
@@ -1,11 +1,11 @@
 package selenium;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,100 +23,94 @@ public class TestSelenium {
 		driver = new ChromeDriver();
 	}
 
+	@AfterClass
+	void tearDown() {
+		driver.close();
+	}
+
 	@BeforeMethod
 	void open() {
-		driver.get("https://accounts.lambdatest.com");
-		driver.manage().window().fullscreen();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		driver.get("http://localhost:3000/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts()
+				.implicitlyWait(Duration.ofSeconds(2));
 	}
 
 	@Test(priority = 0)
-	void signUpFilling() throws InterruptedException {
-		Thread.sleep(5000);
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div/div/div/div[1]/div/div[3]/p/span/span[2]/a"))
-				.click();
-
-		Thread.sleep(5000);
-
-		driver.findElement(By.xpath("//*[@id=\"name\"]"))
-				.sendKeys("Leonardo Angelit Gálvez");
-		driver.findElement(By.xpath("//*[@id=\"email\"]"))
-				.sendKeys("leo63582@gmail.com");
-		driver.findElement(By.xpath("//*[@id=\"userpassword\"]"))
-				.sendKeys("PassPrueba2022!!");
-		driver.findElement(By.xpath("//*[@id=\"phone\"]"))
-				.sendKeys("611438270");
-
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/form/div[5]/label/samp"))
-				.click();
-
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/form/div[6]/button"))
+	void selectCities() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.findElement(By
+				.xpath("//*[@id=\"root\"]/div/div/div/div[3]/button"))
 				.click();
 
 		Thread.sleep(3000);
-	}
-
-	@Test(priority = 1)
-	void lambdaError() throws InterruptedException {
-		driver.findElement(By.xpath("//*[@id=\"email\"]"))
-				.sendKeys("User@hola.com");
-		driver.findElement(By.xpath("//*[@id=\"password\"]"))
-				.sendKeys("rpigjeioj5565");
-
-		driver.findElement(By.xpath("//*[@id=\"login-button\"]"))
-				.click();
-
-		Thread.sleep(10000);
-
-		Assert.assertEquals(driver.findElement(By.xpath(
-				"/html/body/div[1]/div[1]/div/div/div/div[1]/div/form/div[1]/p"))
-				.getText(),
-				"Please enter a correct username and password. Note that the password is case-sensitive");
-	}
-
-	@Test(priority = 2)
-	void loginSuccess() throws InterruptedException {
-		driver.findElement(By.xpath("//*[@id=\"email\"]"))
-				.sendKeys("leonangelitg@gmail.com");
-		driver.findElement(By.xpath("//*[@id=\"password\"]"))
-				.sendKeys("BananaPrueba!123");
-
-		driver.findElement(By.xpath("//*[@id=\"login-button\"]"))
-				.click();
-
-		Thread.sleep(5000);
-
-		driver.findElement(By.xpath("//*[@id=\"profile__dropdown\"]"))
-				.click();
-		driver.findElement(By.xpath(
-				"//*[@id=\"profile__dropdown__parent\"]/div/a[2]"))
-				.click();
-
-		Thread.sleep(5000);
 
 		driver.findElement(By.xpath(
-				"//*[@id=\"profile-panel\"]/div[2]/div[1]/div/form/div[2]/input"))
-				.sendKeys(" GUAPO // HANDSOME");
-		Thread.sleep(8000);
-
-		driver.findElement(By.xpath(
-				"//*[@id=\"profile-panel\"]/div[2]/div[1]/div/form/div[4]/button"))
+				"//*[@id=\"root\"]/div/div[1]/div/div[6]/button"))
 				.click();
 
-		driver.findElement(
-				By.xpath("//*[@id=\"left_sidebar_header\"]/div[1]/a"))
-				.click();
+		final String originCity = driver
+				.findElement(By.xpath("//*[@id=\"trip-origin\"]"))
+				.getDomAttribute("value");
+		final String destinationCity = driver
+				.findElement(
+						By.xpath("//*[@id=\"trip-destination\"]"))
+				.getDomAttribute("value");
 
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 
-		Assert.assertEquals(
-				driver.findElement(By.xpath("//*[@id=\"user-name\"]"))
-						.getText(),
-				"Leonardo GUAPO // HANDSOME");
-
+		Assert.assertEquals(originCity, "Seville");
+		Assert.assertEquals(destinationCity, "Rome");
 	}
+
+	/*
+	 * @Test(priority = 1) void lambdaError() throws InterruptedException {
+	 * driver.findElement(By.xpath("//*[@id=\"email\"]"))
+	 * .sendKeys("User@hola.com");
+	 * driver.findElement(By.xpath("//*[@id=\"password\"]"))
+	 * .sendKeys("rpigjeioj5565");
+	 * 
+	 * driver.findElement(By.xpath("//*[@id=\"login-button\"]")) .click();
+	 * 
+	 * Thread.sleep(10000);
+	 * 
+	 * Assert.assertEquals(driver.findElement(By.xpath(
+	 * "/html/body/div[1]/div[1]/div/div/div/div[1]/div/form/div[1]/p")) .getText(),
+	 * "Please enter a correct username and password. Note that the password is case-sensitive"
+	 * ); }
+	 * 
+	 * @Test(priority = 2) void loginSuccess() throws InterruptedException {
+	 * driver.findElement(By.xpath("//*[@id=\"email\"]"))
+	 * .sendKeys("leonangelitg@gmail.com");
+	 * driver.findElement(By.xpath("//*[@id=\"password\"]"))
+	 * .sendKeys("BananaPrueba!123");
+	 * 
+	 * driver.findElement(By.xpath("//*[@id=\"login-button\"]")) .click();
+	 * 
+	 * Thread.sleep(5000);
+	 * 
+	 * driver.findElement(By.xpath("//*[@id=\"profile__dropdown\"]")) .click();
+	 * driver.findElement(By.xpath(
+	 * "//*[@id=\"profile__dropdown__parent\"]/div/a[2]")) .click();
+	 * 
+	 * Thread.sleep(5000);
+	 * 
+	 * driver.findElement(By.xpath(
+	 * "//*[@id=\"profile-panel\"]/div[2]/div[1]/div/form/div[2]/input"))
+	 * .sendKeys(" GUAPO // HANDSOME"); Thread.sleep(8000);
+	 * 
+	 * driver.findElement(By.xpath(
+	 * "//*[@id=\"profile-panel\"]/div[2]/div[1]/div/form/div[4]/button")) .click();
+	 * 
+	 * driver.findElement( By.xpath("//*[@id=\"left_sidebar_header\"]/div[1]/a"))
+	 * .click();
+	 * 
+	 * Thread.sleep(5000);
+	 * 
+	 * Assert.assertEquals( driver.findElement(By.xpath("//*[@id=\"user-name\"]"))
+	 * .getText(), "Leonardo GUAPO // HANDSOME");
+	 * 
+	 * }
+	 */
 
 }
